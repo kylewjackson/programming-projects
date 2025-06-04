f077b3e49f6171e484a0589878230692a06b4250
Mona Lisa <kylewalkerjackson@gmail.com>
2025-06-04 15:05:45 -0500

    converted Squares to loops

--- React/Tutorials/tic-tac-two/src/App.js
+++ React/Tutorials/tic-tac-two/src/App.js
@@ -33,48 +33,26 @@ function Board({ xIsNext, squares, onPlay }) {
 	return (
 		<>
 			<div className="status">{status}</div>
-			<div className="board-row">
-				<Square
-					value={squares[0]}
-					onSquareClick={() => handleClick(0)}
-				/>
-				<Square
-					value={squares[1]}
-					onSquareClick={() => handleClick(1)}
-				/>
-				<Square
-					value={squares[2]}
-					onSquareClick={() => handleClick(2)}
-				/>
-			</div>
-			<div className="board-row">
-				<Square
-					value={squares[3]}
-					onSquareClick={() => handleClick(3)}
-				/>
-				<Square
-					value={squares[4]}
-					onSquareClick={() => handleClick(4)}
-				/>
-				<Square
-					value={squares[5]}
-					onSquareClick={() => handleClick(5)}
-				/>
-			</div>
-			<div className="board-row">
-				<Square
-					value={squares[6]}
-					onSquareClick={() => handleClick(6)}
-				/>
-				<Square
-					value={squares[7]}
-					onSquareClick={() => handleClick(7)}
-				/>
-				<Square
-					value={squares[8]}
-					onSquareClick={() => handleClick(8)}
-				/>
-			</div>
+			{Array(3)
+				.fill(null)
+				.map((_, row) => {
+					return (
+						<div className="board-row" key={row}>
+							{Array(3)
+								.fill(null)
+								.map((_, cell) => {
+									const i = row * 3 + cell;
+									return (
+										<Square
+											key={i}
+											value={squares[i]}
+											onSquareClick={() => handleClick(i)}
+										/>
+									);
+								})}
+						</div>
+					);
+				})}
 		</>
 	);
 }
